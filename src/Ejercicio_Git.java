5ba93e5a60f24ac50e0dcc7c387a27bd560798f8
Jose <popeluis0003x@hotmail.es>
2022-05-23 09:30:17 +0200

    Clases Creadas

--- src/Ejercicio_Git.java
+++ src/Ejercicio_Git.java
@@ -1,4 +1,22 @@
-
+import java.util.*;
 public class Ejercicio_Git {
+	static Scanner lc= new Scanner(System.in);
+	public static void main(String []args) {
+		int longitud;
+		System.out.println("----------------------------------------------------");
+		System.out.println("Introduce la cantidad de digitos de la 1º Contraseña");
+		longitud=lc.nextInt();
+		System.out.println(Password.getPassword(
+				Password.MINUSCULAS+
+				Password.NUMEROS,longitud));
+		System.out.println("----------------------------------------------------");
+		System.out.println("Introduce la cantidad de digitos de la 2º Contraseña");
+		longitud=lc.nextInt();
+		System.out.println(Password2.getPassword(
+				Password2.MINUSCULAS+
+				Password.NUMEROS+
+				Password2.MAYUSCULAS+
+				Password2.ESPECIALES,longitud));
+		}
+	}
 
-}
